760115d5c4c14cd335eb1cbfd0ae7ac305ebac7c
Sebas <sheva3232@gmail.com>
2023-05-23 22:01:24 -0500

    explorer

--- components/Input.tsx
+++ components/Input.tsx
@@ -3,7 +3,7 @@ import { RiSearch2Line, RiSearchEyeLine, RiSearchLine } from 'react-icons/ri'
 
 const Input = (props: any) => {
 	return (
-		<div className='rounded-full basis-1/4 flex items-center justify-center bg-orange-300'>
+		<div className='rounded-md basis-1/4 flex items-center justify-center bg-slate-500'>
 			<input
 				value={props.input}
 				// placeholder="Enter Contract address"
@@ -13,7 +13,7 @@ const Input = (props: any) => {
 				onChange={e => {
 					props.setInput(e.target.value)
 				}}
-				className='rounded-full bg-orange-300 py-4 px-8 w-11/12 outline-none text-white '
+				className='rounded-md bg-slate-500 py-4 px-8 w-11/12 outline-none text-white '
 			/>
 			<button className='px-3 ' onClick={() => props.search()}>
 				<RiSearch2Line className='text-3xl text-black dark:text-black' />
